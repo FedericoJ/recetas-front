f143c7ddd7e010c03e66ad46292040b49432b044
Tinchin <tincho25.uk@gmail.com>
2022-06-04 11:58:33 -0300

    Ajustes en Login, manejo de errores

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -35,12 +35,20 @@ import {
 
       try {
         const res = await axios.post(`${baseUrl}/usuario/login`,body,setup);
-        navigation.navigate('Principal')
-        console.log(res.data);
-        console.log(res)
+        if (res.status === 201) {
+          navigation.navigate('Principal')
+          console.log(res.data);
+          console.log(res)
+        }
+        if (res.status === 202 || res.status === 203) {
+          console.log("Error")
+          console.log(res.data);
+          console.log(res)
+          alert("Invalid username or password")
+        }
       }catch(error){
         console.log("Error")
-        alert("Error de login")
+        alert("Servicio no disponible")
       }
     }
     ;
