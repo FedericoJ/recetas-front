11b223fd8b8e261fc969eedfdf4085046c303772
Gonzalo Esperidioni <gonzaloesperidioni@gmail.com>
2022-06-04 23:09:21 -0300

    hola

--- src/screens/Principal.js
+++ src/screens/Principal.js
@@ -1,50 +1,42 @@
-import React from 'react';
-import { ScrollView,StyleSheet} from 'react-native';
-import { Divider, Flex, Box, Heading, Center,NativeBaseProvider,Text,Button } from "native-base";
-import Tabs from '../components/Tabs';
-import Galeria from '../components/Carousel';
-import Categorias from '../components/Categorias'
-import { FloatingAction } from "react-native-floating-action";
-import { FAB } from 'react-native-paper';
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import Tabs from "../components/Tabs";
+import Galeria from "../components/Carousel";
+import Categorias from "../components/Categorias";
+import { FAB } from "react-native-paper";
 
-
-
-const Principal  = ({navigation}) => {
-
-
-return (
+const Principal = ({ navigation }) => {
+  return (
     <ScrollView style={styles.container}>
-        <Tabs/>
-        <Galeria navegacion={navigation}/>
-        <Categorias/>
+      <Tabs />
+      <Galeria navegacion={navigation} />
+      <Categorias />
 
-         <FAB style={styles.fab}
-          extended
-          icon="pencil"
-          label ="Nueva"
-          uppercase={false}
-          onPress={() => console.log('Pressed')}
-          />
-
-     </ScrollView>
+      <FAB
+        style={styles.fab}
+        extended
+        icon="pencil"
+        label="Nueva"
+        uppercase={false}
+        onPress={() => console.log("Pressed")}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor:'#D6B1B1'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#D6B1B1",
+  },
 
-    fab: {
-      position: 'absolute',
-      margin: 16,
-      right: 0,
-      bottom: 0,
-      backgroundColor:"#FFD700"
-      
-    },
-
-  });
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#FFD700",
+  },
+});
 
 export default Principal;
