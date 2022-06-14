e67d6bbd210b075f4b0b91f1ce1036493559e6cb
germainero <german.mainero@outlook.com>
2022-06-14 12:21:50 -0300

    crear receta

---
+++ src/components/UploadImagePaso.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Image, View, Platform, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function UploadImagePaso() {
+ const [image, setImage] = useState(null);
+ const addImage=()=>{};
+
+ return (
+<View style={imageUploaderStyles.container}>
+               {
+                   image  &&<Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
+               }
+
+<View style={imageUploaderStyles.uploadBtnContainer}>
+<TouchableOpacity onPress={addImage} style={imageUploaderStyles.uploadBtn} >
+{/* <Text>{image ? 'Edita' : 'Carga'}</Text> */}
+<AntDesign name="camera" size={20} color="black" />
+</TouchableOpacity>
+</View>
+
+
+</View>
+
+ );
+}
+
+const imageUploaderStyles=StyleSheet.create({
+   container:{
+       elevation:2,
+       height:90,
+    //    width:150,
+       backgroundColor:'#efefef',
+       position:'relative',
+    //    borderRadius:999,
+       overflow:'hidden',
+   },
+   uploadBtnContainer:{
+       opacity:0.7,
+       position:'absolute',
+       right:0,
+       bottom:0,
+       backgroundColor:'lightgrey',
+       width:'100%',
+       height:'30%',
+   },
+   uploadBtn:{
+       display:'flex',
+       alignItems:"center",
+       justifyContent:'center'
+   }
+})
+
+
--- src/screens/CreateReceta.js
+++ src/screens/CreateReceta.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import {  ScrollView,  StyleSheet,  Image,  View,  Text,  TouchableOpacity,  TextInput,  Modal,} from "react-native";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import { NativeBaseProvider, TextArea, Input, Divider, FormControl,VStack, HStack,Select,CheckIcon } from "native-base";
-import TipoComidas from "../components/TipoComida";
 import { ButtonModal, ButtonFondoBlanco, ButtonFondoRosa, ButtonCreateBlanco,ButtonCreateRosa } from "../components/ButtonsLogin";
-import Box from "@mui/material/Box";
-import Steps from "../components/Step";
 import UploadImageReceta from '../components/UploadImageReceta';
+import UploadImagePaso from '../components/UploadImagePaso';
 import { useNavigation } from '@react-navigation/native';
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+import { mdiPlusCircleOutline } from '@mdi/js';
+import Icon from '@mdi/react'
 
 
 const CreateReceta = () => {
@@ -20,6 +20,10 @@ const CreateReceta = () => {
   const [porciones , setPorciones] = useState("");
   const [personas , setPersonas] = useState("");
   const [categoriaSel,setCategoriaSel]=useState("1");
+  const [unidadSel,setUnidadesSel]=useState("1");
+  const [ingrediente , setIngrediente] = useState("");
+  const [cantidad , setCantidad] = useState("");
+  const [descPaso , setdescPaso] = useState("");
 
   const [categorias, setCategorias] = useState([
     { label: "Pasta", value: "1" },
@@ -31,13 +35,22 @@ const CreateReceta = () => {
     setCategoriaSel(item);
   };
 
+  const [unidades, setUnidades] = useState([
+    { label: "Kg", value: "1" },
+    { label: "Gr", value: "2" },
+    { label: "Lt", value: "3" },
+  ]);
+
+  const onChangeHandler2 = (item) => {
+    setUnidadesSel(item);
+  };
+
+
 
 
   return (
     <ScrollView style={styles.container}>
            <NativeBaseProvider>
-
-         {/* <View style={{flex: 1,progressBarColor: "red",completedProgressBarColor:"red"}}> */}
           <ProgressSteps>
               <ProgressStep label="Descripción">
                <ScrollView style={styles.container}>
@@ -73,7 +86,9 @@ const CreateReceta = () => {
                         placeholder="Titulo"
                         backgroundColor="#FFFF"
                         value={titulo}
-                        onChangeText={setTitulo}/>
+                        onChangeText={setTitulo}
+                        fontSize= "25"/>
+                        
                     </FormControl>
 
                 </View>
@@ -91,28 +106,33 @@ const CreateReceta = () => {
                 </View>
 
                   <TextArea
-                    style={{ backgroundColor: "#ffff" }}
+                    style={{ backgroundColor: "#ffff"}}
                     w="90%"
                     mx="5"
                     placeholder="Descripcion"
                     value={descripcion}
                     onChangeText={setDescripcion}
+                    fontSize= "20"
+                    marginBottom="10%"        
                   />
-
                   
                   <HStack mt="2" w="100%"  >
-                    <Text style={{ fontSize: 16, marginLeft:"5%" ,width:"45%" }}> Categoria </Text>
+                    <Text style={{ fontSize: 20, marginLeft:"5%" ,width:"45%" }}> Categoria </Text>
 
                     <Select
                         style={{backgroundColor:'#ffff'}}
                         selectedValue={categoriaSel}
+                        marginRight="10%"
+                        w="70%"
+                        fontSize= "16"
                         mr="2"
                         alignSelf="flex-end"
                         _selectedItem={{
                           bg: "indigo",
-                          endIcon: <CheckIcon size={5} />,
+                          endIcon: <CheckIcon size={5}  />,
                         }}
                         onValueChange={onChangeHandler}
+                        
                       >
                         {categorias.map((categoria) => (
                           <Select.Item
@@ -133,13 +153,14 @@ const CreateReceta = () => {
                       marginHorizontal: "5%",
                     }}
                   >
-                    <Text style={{ fontSize: 16, width: "90%" }}> Porciones </Text>
+                    <Text style={{ fontSize: 20, width: "90%" }}> Porciones </Text>
 
                     <Input
                       style={{ backgroundColor: "#ffff", textAlign: "center" }}
                       mx="1"
-                      w="15%"
-                      placeholder=""
+                      w="44%"
+                      fontSize= "16"
+                      placeholder="Cantidad"
                       value={porciones}
                       onChangeText={setPorciones}
                     />
@@ -154,14 +175,15 @@ const CreateReceta = () => {
                       marginHorizontal: "5%",
                     }}
                   >
-                    <Text style={{ fontSize: 16, width: "90%" }}> Personas </Text>
+                    <Text style={{ fontSize: 20, width: "90%" }}> Personas </Text>
 
                     <Input
                       style={{ backgroundColor: "#ffff", textAlign: "center" }}
                       mx="1"
-                      w="15%"
-                      placeholder=""
+                      w="44%"
+                      placeholder="Cantidad"
                       value={personas}
+                      fontSize= "16"
                       onChangeText={setPersonas}
                     />
                   </View>
@@ -183,6 +205,70 @@ const CreateReceta = () => {
                     {" "}
                     Ingredientes{" "}
                   </Text>
+                  <HStack mt="3" w="90%"  >
+                  <Input
+                      style={{ backgroundColor: "#ffff", textAlign: "center" }}
+                      mx="1"
+                      w="44%"
+                      fontSize= "16"
+                      placeholder="Ingrediente"
+                      value={ingrediente}
+                      onChangeText={setIngrediente}
+                      marginLeft="5%"
+                    />
+                     <Input
+                      style={{ backgroundColor: "#ffff", textAlign: "center" }}
+                      mx="1"
+                      w="21%"
+                      fontSize= "16"
+                      placeholder="Cant"
+                      value={cantidad}
+                      onChangeText={setCantidad}
+                    />
+
+                    <Select
+                        style={{backgroundColor:'#ffff'}}
+                        selectedValue={unidadSel}
+                        marginRight="47%"
+                        w="50%"
+                        fontSize= "16"
+                        mr="2"
+                        alignSelf="flex-end"
+                        _selectedItem={{
+                          bg: "indigo",
+                          endIcon: <CheckIcon size={1}  />,
+                        }}
+                        onValueChange={onChangeHandler2}
+                        
+                      >
+                        {unidades.map((unidad) => (
+                          <Select.Item
+                            key={unidad.value}
+                            label={unidad.label}
+                            value={unidad.value}
+                          />
+                        ))}
+                    </Select>
+
+                  </HStack>
+                  <TouchableOpacity>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "flex-end",
+                      marginTop: "5%",
+                      marginBottom: "2%",
+                      marginHorizontal: "20%",
+                    }}
+                  >
+                  <Icon path={mdiPlusCircleOutline}
+                      title="User Profile"
+                      size={1}
+                      color="black"/>
+                  
+                  <Text style={{ fontSize: 20, width: "90%", fontWeight: "bold" }}> Agregar Ingrediente </Text>  
+                  </View>
+                  </TouchableOpacity>
                   </NativeBaseProvider>
               </ScrollView>
               </ProgressStep>
@@ -204,11 +290,46 @@ const CreateReceta = () => {
                     {" "}
                     Pasos{" "}
                   </Text>
+
+                  <TextArea
+                    style={{ backgroundColor: "#ffff"}}
+                    w="90%"
+                    mx="5"
+                    placeholder="Descripcion"
+                    value={descPaso}
+                    onChangeText={setdescPaso}
+                    fontSize= "20"
+                    marginBottom="10%"        
+                  />
+              <View style={styles.imagenPaso}>
+              <View style={{ width: "20%", marginLeft:"5%"}}>
+                  <UploadImagePaso/>
+                  </View>
+                   </View>
+
+                  <TouchableOpacity>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "flex-end",
+                      marginTop: "10%",
+                      marginBottom: "2%",
+                      marginHorizontal: "30%",
+                    }}
+                  >
+
+                  <Icon path={mdiPlusCircleOutline}
+                      title="User Profile"
+                      size={1}
+                      color="black"/>
+                  
+                  <Text style={{ fontSize: 20, width: "90%", fontWeight: "bold" }}> Agregar Paso </Text>  
+                  </View>
+                  </TouchableOpacity>
                   </NativeBaseProvider>
               </ScrollView>
               </ProgressStep>
           </ProgressSteps>
-      {/* </View> */}
 
       </NativeBaseProvider>
     </ScrollView>
@@ -226,6 +347,12 @@ const styles = StyleSheet.create({
     backgroundColor:"#D6B1B1",
     justifyContent:"center",
   },
+  imagenPaso: {
+    flexDirection:"row",
+    backgroundColor:"#D6B1B1",
+    justifyContent:"left",
+  },
+
 });
 
 export default () => {
