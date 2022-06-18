319b7391604c9dbc9203bcc5137b78a256936526
chrisbreitman <christianbreitman@gmail.com>
2022-06-18 13:41:45 -0300

    cambiamos componente react a componente de clase

--- src/components/Step.js
+++ src/components/Step.js
@@ -2,8 +2,11 @@ import * as React from 'react';
 import {View,Text,Image,TouchableOpacity} from 'react-native';
 import Stars from 'react-native-stars';
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons'; 
-import {NativeBaseProvider,TextArea,Input,Select,CheckIcon,HStack} from "native-base";
+import {NativeBaseProvider,TextArea,Input,Select,CheckIcon,HStack,Spinner} from "native-base";
 import ImageView from 'react-native-image-view';
+import axios from 'axios'
+import config from "../config/default.json";
+import useSWR from 'swr'
 
     var images =[]
 
@@ -23,10 +26,16 @@ import ImageView from 'react-native-image-view';
     }
 
 
-const ViewImagesAndVideo = ({imagenes},{indice}) => {
+const ViewImagesAndVideo = ({idPaso},{indice}) => {
 
     const [galeriaSeleccionada, setgaleriaSeleccionada] = React.useState(false);
     
+    const imagenes = [{
+        url: 'https://cdn.pixabay.com/photo/2017/08/17/10/47/paris-2650808_960_720.jpg',extension:"jpg"},
+        {url: 'https://d320djwtwnl5uo.cloudfront.net/recetas/cover/milan_SuLEW9PUrTwyi0npoGIKD5zNqHmcAb.png',extension:"mp4"},
+        {url: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/05/25115909/hamburguesa-destacada.jpg',extension:"jpg"}]
+
+
     
     armarView(imagenes);
   
@@ -63,44 +72,65 @@ const ViewImagesAndVideo = ({imagenes},{indice}) => {
   }
 
 
-const Steps = ({ pasos }) => {
+const Steps = ({Receta}) => {
 
-        const Imagenes = [{
-            url: 'https://cdn.pixabay.com/photo/2017/08/17/10/47/paris-2650808_960_720.jpg',extension:"jpg"},
-            {url: 'https://d320djwtwnl5uo.cloudfront.net/recetas/cover/milan_SuLEW9PUrTwyi0npoGIKD5zNqHmcAb.png',extension:"mp4"},
-            {url: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/05/25115909/hamburguesa-destacada.jpg',extension:"jpg"}]
 
-        return (
-            <View>
-               {pasos &&
-                pasos.map((element, i) => (
+    const pasos = [
+        { idPaso: 1, descripcion: "Aca cortamos la cebolla en juliana" },
+        {
+          idPaso: 2,
+          descripcion: "Abrimos la  pechuga al medio como le gusta a godios",
+        },
+        { idPaso: 3, descripcion: "Agregamos sal a piacere" },
+      ];
 
-                    <View key ={i}>
+    const baseUrl =  config.baseUrl;
 
-                        <View  style={{flexDirection:"row" ,alignItems:"flex-start", marginTop:'5%', marginBottom:'2%', marginLeft:'5%'}}>
+    const fetcher = url => axios.get(`${baseUrl}/receta/getPasos?idReceta=${Receta}`).then(res => res.data)
 
-                            <Text  style={{fontSize:20, fontWeight:"bold"}}> {element.idPaso} </Text> 
+    const pasito=useSWR(`${baseUrl}/receta/getPasos?idReceta=${Receta}`, fetcher);
 
-                            <NativeBaseProvider >
+    if(pasito.data) console.log(pasito.data);
 
-                                <TextArea  style={{backgroundColor:"#ffff"}} w="90%" mr="5" ml ="2" value={element.descripcion} 
-                                    placeholder="Disabled TextArea" isDisabled />
+
+        if (!pasito.data){
+            return( <NativeBaseProvider>
+                    <HStack space={8} justifyContent="center">
+                      <Spinner color="warning.500" />
+                    </HStack>
+                    </NativeBaseProvider>)
+          }else{    
+
+                return (
+                    <View>
+                        {pasito.data &&
+                        pasito.data.map((element, i) => (
+
+                            <View key ={i}>
+
+                                <View  style={{flexDirection:"row" ,alignItems:"flex-start", marginTop:'5%', marginBottom:'2%', marginLeft:'5%'}}>
+
+                                    <Text  style={{fontSize:20, fontWeight:"bold"}}> {element.idPaso} </Text> 
+
+                                    <NativeBaseProvider >
+
+                                        <TextArea  style={{backgroundColor:"#ffff"}} w="90%" mr="5" ml ="2" value={element.texto} 
+                                            placeholder="Disabled TextArea" isDisabled />
+                                        
+                                    </NativeBaseProvider>
+
+
+                                </View>
+
+                                <ViewImagesAndVideo imagenes={element.idPaso} indice={i}/>
+
+                            </View>
                                 
-                            </NativeBaseProvider>
-
-
-                        </View>
-
-                        <ViewImagesAndVideo imagenes={Imagenes} indice={i}/>
-
-                    </View>
-                        
-                ))}
-
-
-              </View>    
-            
-        );
+                        ))}
+                    </View>    
+        
+                );
+        }
 
     }
 
