48560775f503da56508be96e11404b0cf17028d0
chrisbreitman <christianbreitman@gmail.com>
2022-06-18 15:37:42 -0300

    merge

--- src/components/Recetas.js
+++ src/components/Recetas.js
@@ -29,55 +29,24 @@ const ModalPoup = ({ visible, children }) => {
   
   };
 
-const Recetas=({tipos}) =>{
-
-    const netInfo = useNetInfo();
-    const [noWifi, setNoWifi] = React.useState(false);
-
-/*CalificacionProm: "4.0000"
-Descripcion: "Budin de naranja con semillas de amapola"
-IdReceta: 1
-Nombre: "Budin naranja"
-alias: "mrv"
-foto: "https://www.clarin.com/img/2019/07/23/bs56zWpBq_1256x620__2.jpg#1591385524188"*/
 
 const recetasHandler=(tipos,navigation) =>{
 
     variables.setTipos(tipos); 
-    setNoWifi(true);
-    navigation.navigate('Receta')
+    navigation.navigate('Receta');
+    
 }
 
 
 const Recetas=({tipos}) =>{
-    const wifi = () => {
-      if(!netInfo.type === "wifi"){
-        navigation.navigate('Receta',{datos:tipos})
-      }
-      else {
-        setNoWifi(true)
-      }
-    };
-  
 
         const {Descripcion,IdReceta,Nombre,alias,foto,CalificacionProm} = tipos;
         const  navigation  = useNavigation();
+        const netInfo = useNetInfo();
+        const [noWifi, setNoWifi] = React.useState(false);
 
-        
 
-        return (<View>
-            
-                <ModalPoup visible={noWifi}>
-                <View style={{ alignItems: 'flex-start' }}>
-                <Text style={{ fontSize: 20, color: "black" }}>No se encuentra conectado a una red Wifi. ¿Desea continuar usando sus datos? </Text>
-                <View style={{ flexDirection: "row", alignItems: "center", marginTop: '2%', marginBottom: '2%', marginHorizontal: '5%' }}>
-                </View>
-                </View>
-                <View style={{ flexDirection: "row", alignItems: "center", marginTop: '1%', marginBottom: '1%', marginHorizontal: '1%' }}>
-                <ButtonModal text="Cancelar" onPress={() => { navigation.navigate('Principal'); setNoWifi(false); }} />
-                <ButtonModal text="Aceptar" onPress={() => { navigation.navigate('Receta',{datos:tipos});setNoWifi(false) }} />
-                </View>
-            </ModalPoup>
+        return (
 
             <TouchableOpacity style={{backgroundColor:'#ffff',marginVertical:'2%',marginRight:"8%"}} onPress = { () => recetasHandler(tipos,navigation)}>
                 
@@ -122,7 +91,6 @@ const Recetas=({tipos}) =>{
                         </View>
                 </View>
             </TouchableOpacity>  
-            </View>  
         )
     }
 
