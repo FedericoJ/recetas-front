c245558f1c5f22cd2cf429267a5c74e7fb10323d
fede <17federico@gmail.com>
2022-06-20 20:09:40 -0300

    Navigation Final Version

--- src/navigation/RecetaStackNavigator.js
+++ src/navigation/RecetaStackNavigator.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Receta from "../screens/ViewReceta";
+import CalcularReceta from "../screens/CalcularReceta";
 const Stack = createStackNavigator();
 
 const RecetaStackNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name = "Receta" component={Receta} />
+            <Stack.Screen name = "CalcularReceta" component={CalcularReceta} />
         </Stack.Navigator>
     )
 }
