0c83865217a3edfaea9cf6f0e03b6ad7ee2f048d
fede <17federico@gmail.com>
2022-06-21 18:44:09 -0300

    Modifico perfil screen

--- src/navigation/EditarPerfilStackNavigator.js
+++ src/navigation/EditarPerfilStackNavigator.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import EditarPerfilScreen from "../screens/EditarPerfilScreen";
+import PerfilScreen from "../screens/PerfilScreen";
+
 const Stack = createStackNavigator();
 
 const EditarPerfilStackNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name = "PerfilScreen" component={PerfilScreen} />
             <Stack.Screen name = "EditarPerfilScreen" component={EditarPerfilScreen} />
         </Stack.Navigator>
     )
