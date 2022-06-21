987e03d76999beffa583c9b85ea269c22fcb41d3
fede <17federico@gmail.com>
2022-06-21 18:48:57 -0300

    Cambio en editar perfil

--- src/navigation/EditarPerfilStackNavigator.js
+++ src/navigation/EditarPerfilStackNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import EditarPerfilScreen from "../screens/EditarPerfilScreen";
 import PerfilScreen from "../screens/PerfilScreen";
+import CambiarContraseniaScreen from "../screens/CambiarContraseniaScreen";
 
 const Stack = createStackNavigator();
 
@@ -9,7 +10,11 @@ const EditarPerfilStackNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name = "PerfilScreen" component={PerfilScreen} />
-            <Stack.Screen name = "EditarPerfilScreen" component={EditarPerfilScreen} />
+            <Stack.Screen name = "EditarPerfil" component={EditarPerfilScreen} />
+            <Stack.Screen
+              name="CambiarContrasenia"
+              component={CambiarContraseniaScreen}
+            />
         </Stack.Navigator>
     )
 }
